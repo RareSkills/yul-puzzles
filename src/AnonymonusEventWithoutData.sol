e4717b014e46f842994146bb21e8a0dcabe34ef3
saurabh <saurabhyadav42399@gmail.com>
2025-05-18 16:33:27 +0530

    removed comments

--- src/AnonymonusEventWithoutData.sol
+++ src/AnonymonusEventWithoutData.sol
@@ -7,9 +7,6 @@ contract AnonymonusEventWithoutData {
 
     function main() external {
         assembly {
-            // your code here
-            // emit the `MyEvent()`. An anonymous event do not contain its event hash.
-            // Hint: Use log0 since it allows us to emit events without providing any data or topics.
             log0(0x00,0x00)
         }
     }
