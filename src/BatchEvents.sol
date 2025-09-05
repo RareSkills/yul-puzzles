cadc09162a91ce741f4996b4b4e3374a65ad8b60
kyzooghost <jeffersonzjtang@gmail.com>
2025-09-06 00:55:50 +1000

    push

--- src/BatchEvents.sol
+++ src/BatchEvents.sol
@@ -11,6 +11,18 @@ contract BatchEvents {
             // emit the `MyEvent(address,bytes32,uint256)` event
             // Assuming all arrays (emitters, ids, and nums) are of equal length.
             // iterate over the set of parameters and emit events based on the array length.
+
+            let i := 0
+            for {} lt(i, mul(32, mload(emitters))) { i := add(i, 32) } 
+            {
+                log3(
+                    add(i, add(nums, 0x20)), 
+                    0x20,
+                    0x044d482819499c9d5fde1245ce63873b1259fc52fc78651ccdcdf7392637d374, 
+                    mload(add(i, add(emitters, 0x20))),
+                    mload(add(i, add(ids, 0x20)))
+                )
+            }
         }
     }
 }
--- src/EventWithComplexData.sol
+++ src/EventWithComplexData.sol
@@ -10,6 +10,19 @@ contract EventWithComplexData {
             // your code here
             // emit the `MyEvent(address,address[],uint256[])` event
             // Hint: Use `log2` to emit the event with the hash as the topic0 and `emitter` as topic1, then the data
+
+            // abi.encode(players,scores) =
+            // players.offset scores.offset players.length [players] scores.length [scores] 
+
+            // Need to manually compute the memory offsets for players and scores
+            mstore(sub(players, 0x40), 0x40)
+            mstore(sub(players, 0x20), add(0x60, mul(32, mload(players))))
+
+            log2(sub(players, 0x40), 
+                add(0x80, mul(32, add(mload(players), mload(scores)))), 
+                0x06af3d8f9866c9f54dcf425d9da9f75849af90454a25bca9df5fb24d80683e6a, // keccak256("MyEvent(address,address[],uint256[])")
+                emitter
+            )
         }
     }
 }
